9d1a3509251ff6157a977ccc1d367e1be51152c0
kartavyashishangiya1409 <kartavyashishangiya1409@gmail.com>
2025-11-29 19:44:24 +0530

    Switched to MongoDB Atlas

--- .history/Server/index_20251129194303.js
+++ .history/Server/index_20251129194303.js
@@ -199,5 +199,5 @@ app.post("/deleteItem", async (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log("Server running on port ", PORT);
+    console.log("Server running on port", PORT);
 });
